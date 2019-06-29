c9c734596be3e718fb1c7fbdb0bd08cba57d05e4
Tirta Nugraha <dev.nugrata@gmail.com>
2019-06-29 16:01:18 +0700

    style(css): remove unnecessary logo

--- shared/container/DemoApp/Header/index.js
+++ shared/container/DemoApp/Header/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
-import Logo from './Logo';
 import Menu from './Menu';
-
-const StyledTitle = styled.h1`
-  background: transparent;
-  color: #9b56d0;
-`;
 
 function Header() {
   return (
-    <div style={{ textAlign: 'center', marginBottom: '1rem' }}>
-      <Logo />
-      <StyledTitle>React, Start</StyledTitle>
-      <strong>Heavily modified of react universally</strong>
-      <Menu />
-    </div>
+    <Menu />
   );
 }
 
