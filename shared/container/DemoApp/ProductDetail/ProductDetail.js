80db0e581e8247dd8feacd74c98d4d2e84e54bdb
Tirta Nugraha <dev.nugrata@gmail.com>
2019-06-30 20:52:25 +0700

    refactor(product): refactor product detail page

--- shared/container/DemoApp/ProductDetail/ProductDetail.js
+++ shared/container/DemoApp/ProductDetail/ProductDetail.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import firebase from '../Firebase';
+import { BackButton } from './ProductDetailStyle';
+import ProductDetail from '../../../components/Products/ProductDetail';
 
 let db = firebase.firestore();
 
-class ProductDetail extends React.Component {
+class ProductDetailPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       name: '',
       image: '',
       price: '',
+      isProductFound: false,
     };
+
+    this.goBack = this.goBack.bind(this);
   }
+
+  goBack() {
+    this.props.history.goBack();
+  }
+
   componentDidMount() {
-    const itemRef = db.collection("product").doc("dMi95Mwn9TevydyYuQuc");
+    const dataKey = this.props.match.params.productKey;
+    const itemRef = db.collection("product").doc(dataKey);
     itemRef.get().then(doc => {
       const itemData = doc.data();
       if(doc.exists) {
@@ -22,21 +35,29 @@ class ProductDetail extends React.Component {
             name: itemData.productName,
             image: itemData.productImage,
             price: itemData.productPrice,
+            isProductFound: true,
           }
         });
-
-        console.log('state : ', this.state);
+      } else {
+        this.setState((prevState, props) => {
+          return {
+            isProductFound: false,
+          }
+        })
       }
     })
   }
   render() {
     return (
       <React.Fragment>
-        product detail
+        <BackButton onClick={this.goBack}>
+          {`<<  back`}
+        </BackButton>
+        <ProductDetail data={this.state} />
       </React.Fragment>
     );
   }
 }
 
-export default ProductDetail;
+export default ProductDetailPage;
 
