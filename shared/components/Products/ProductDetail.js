6b319bb31aca11907e8b23e5048c3638571c1c83
Tirta Nugraha <dev.nugrata@gmail.com>
2019-06-30 21:43:52 +0700

    style(product): product detail page style adjustment

--- shared/components/Products/ProductDetail.js
+++ shared/components/Products/ProductDetail.js
@@ -5,7 +5,14 @@ import formatNumber from '../../utils/currencyFormat';
 
 const ProductName = styled.h1`
   margin-top: 0
+  margin-bottom: 8px;
   line-height: 30px;
+  font-size: 28px;
+  font-weight: 800;
+`;
+
+const ProductDescription = styled.div`
+  margin-top: 28px;
 `;
 
 const ProductList = props => {
@@ -20,7 +27,12 @@ const ProductList = props => {
 
         <div className="col-xs-12 col-md-9 col-lg-9">
           <ProductName>{props.data.name}</ProductName>
-          <h3>{formatNumber(props.data.price)}</h3>
+          <span class="tag is-dark is-medium">Rp.&nbsp;{formatNumber(props.data.price)}</span>
+
+          <ProductDescription>
+            <span className="tag is-link is-normal">product description</span>
+            <p>{props.data.description}</p>
+          </ProductDescription>
         </div>
       </div>
     </React.Fragment>
